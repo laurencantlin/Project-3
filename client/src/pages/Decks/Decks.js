082b48ad49f80be6bd566234d7106028697c029d
laurencantlin <laurenrose5321@gmail.com>
2018-03-23 18:39:00 -0500

    links to database removes favi and adds to pages

--- client/src/pages/Decks/Decks.js
+++ client/src/pages/Decks/Decks.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import API from "../../utils/API";
+// import API from "../../utils/API";
 // import { Link } from "react-router-dom";
-import { Deck } from "../../components/Decks"
+import Deck from "../../components/Decks"
 // import { Col, Row, Container } from "../../components/Grid";
 // import { List, ListItem } from "../../components/List";
 // import { Input, TextArea, FormBtn } from "../../components/Form";
@@ -9,7 +9,9 @@ import { Deck } from "../../components/Decks"
 class Decks extends Component {
   render() {
     return (
-      <div>Deck</div>
+      <div>Deck
+        <Deck/>
+      </div>
     );
   }
 }
